3c34c5cde7a1e14aa7b9beaecf0a652d199abbb3
Nicklas Winther <nicklas2002@hotmail.com>
2024-05-09 18:41:47 +0200

    up

--- src/main/java/org/example/config/ApplicationConfig.java
+++ src/main/java/org/example/config/ApplicationConfig.java
@@ -129,12 +129,12 @@ public class ApplicationConfig {
 
     public ApplicationConfig configureCors() {
         app.before(ctx -> {
-            ctx.header("Access-Control-Allow-Origin", "");
+            ctx.header("Access-Control-Allow-Origin", "*");
             ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             ctx.header("Access-Control-Allow-Headers", "Content-Type");
         });
 
-        app.options("/", ctx -> {
+        app.options("/*", ctx -> {
             ctx.header("Access-Control-Allow-Origin", "*");
             ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
             ctx.header("Access-Control-Allow-Headers", "Content-Type");
